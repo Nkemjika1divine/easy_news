09df6702aec324d9acfb462b488de3c653b9cd1f
Nkemjika1divine <cndivine@gmail.com>
2024-08-22 00:19:49 +0100

    User.is_valid_password() added

--- models/user.py
+++ models/user.py
@@ -21,3 +21,12 @@ class User(BaseModel, Base):
         if not password or type(password) is not str:
             return None
         return hashpw(password.encode("utf8"), gensalt())
+    
+
+    def is_valid_password(self, password: str = None) -> bool:
+        """Verifies to ensure that password entered is the same in the DB"""
+        if not password or type(password) is not str:
+            return False
+        if self.password is None:
+            return False
+        return checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
