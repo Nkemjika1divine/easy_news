a8dee13ecd762670ba62cb877aff6f85061cef19
Nkemjika1divine <cndivine@gmail.com>
2024-08-22 17:41:15 +0100

    Auth.require_auth() added

--- auth/auth.py
+++ auth/auth.py
@@ -39,3 +39,27 @@ class Auth():
             return request.headers
         else:
             return {}
+        
+    
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication.
+        -   If path is in excluded_paths, it does not require authentication"""
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
+        else:
+            path_with_slash = path + '/'
+            if path_with_slash in excluded_paths:
+                return False
+            else:
+                for paths in excluded_paths:
+                    if paths[-1] == '*':
+                        count = 0
+                        for i in paths:
+                            count += 1
+                            if path[0:count - 1] == paths[0:-1]:
+                                return False
+                return True
