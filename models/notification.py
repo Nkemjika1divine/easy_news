e06cf6305941547942df474ef612b0b14e7efaf2
Nkemjika1divine <cndivine@gmail.com>
2024-08-22 02:26:50 +0100

    follow.py created; Notification model created

---
+++ models/notification.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The Notification module"""
+from models.basemodel import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+
+
+class Notification(BaseModel, Base):
+    """The Notification Model"""
+    __tablename__ = "notifications"
+    user_id = Column(String(50), ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
+    channel_id = Column(String(50), ForeignKey("channels.id", ondelete='CASCADE'), nullable=False)
+    article_title = Column(String(500), nullable=False)
+    article_description = Column(String(1000), nullable=True)
+    article_url = Column(String(1000), nullable=False)
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
