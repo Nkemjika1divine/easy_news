5a790ee49060e22e2ff02c8249cf5896f150a02e
Nkemjika1divine <cndivine@gmail.com>
2024-08-23 20:20:23 +0100

    POST /api/v1/users/register updated to generate sesssion ids and set cookie

--- api/v1/endpoints/users.py
+++ api/v1/endpoints/users.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Module containing /users* endpoints"""
 from api.v1.error_handlers import *
+from datetime import datetime, timezone, timedelta
 from fastapi import APIRouter, Request, status
 from fastapi.responses import JSONResponse
 from models.user import User
+from os import environ
 from utils.utility import validate_email_pattern
 
 
@@ -13,6 +15,7 @@ user_router = APIRouter()
 @user_router.post("/users/register")
 async def register_user(request: Request) -> str:
     """POST request to register a user"""
+    from auth.auth import Auth
     from models import storage
     if not request:
         raise Bad_Request()
@@ -42,6 +45,13 @@ async def register_user(request: Request) -> str:
         raise Bad_Request("Password length must be greater than 7 and less than 21")
     
     user = User(name=name, email=email, password=password, role=role)
-    storage.new(user)
-    storage.save()
-    return JSONResponse(content=user.to_safe_dict(), status_code=status.HTTP_201_CREATED)
+    user.save()
+    auth = Auth()
+    session_id = auth.create_session(user.id)
+    expiry_date = datetime.now(timezone.utc) + timedelta(days=30)
+    response = JSONResponse(content=user.to_safe_dict(), status_code=status.HTTP_201_CREATED)
+    response.set_cookie(key=environ.get("SESSION_NAME"), value=session_id, expires=expiry_date)
+    return response
+
+
+
