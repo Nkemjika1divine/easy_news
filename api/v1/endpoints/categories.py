135773c56a48f10bedf148acc93fe1ef0dcba6cf
Nkemjika1divine <cndivine@gmail.com>
2024-08-28 01:23:39 +0100

    PUT /api/v1/categories/{category_id} added

--- api/v1/endpoints/categories.py
+++ api/v1/endpoints/categories.py
@@ -26,4 +26,29 @@ async def add_a_category(request: Request) -> str:
     category = Category()
     category.category_name = category_name.lower()
     category.save()
-    return JSONResponse(content=f"{category_name} saved successfully")
+    return JSONResponse(content=f"{category_name} saved successfully", status_code=status.HTTP_200_OK)
+
+
+@categories_router.put("/categories/{category_id}")
+async def add_a_category(request: Request, category_id: str = None) -> str:
+    """POST method that adds a new category"""
+    from models import storage
+    if not request:
+        raise Bad_Request()
+    if not category_id:
+        raise Not_Found()
+    if not request.state.current_user:
+        raise Unauthorized()
+    try:
+        request_body = await request.json()
+    except Exception as error:
+        raise Bad_Request(error)
+    category = storage.search_key_value("Category", "id", category_id)
+    if not category:
+        raise Not_Found("Category does not exist")
+    category_name = request_body.get("name", None)
+    if not category_name or type(category_name) is not str or len(category_name) > 50:
+        raise Bad_Request("category name must be a string and must not exceed 50 characters")
+    category.category_name = category_name.lower()
+    storage.save()
+    return JSONResponse(content=f"{category_name} updated successfully", status_code=status.HTTP_200_OK)
